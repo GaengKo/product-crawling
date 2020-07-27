fcda4224c9e49c95cd8c222ff2258a7b6bbbc9cd
gaengko <gaengko@korea.ac.kr>
2020-07-27 17:16:56 +0900

    update str replace

--- amazon_strReplace.py
+++ amazon_strReplace.py
@@ -7,25 +7,40 @@ f = open('./amazon_review/Roborock S5 Robot.xlsx','r',encoding='cp949')
 rdr = csv.reader(f)
 word = {}
 rdr = pd.read_excel('./amazon_review/Roborock S5 Robot.xlsx')
-print(rdr.head)
-print('')
-for line in rdr:
-    if line[0] != 'title':
-        print(line)
+
+print(rdr.iloc[0,1])
+print(len(rdr))
+for i in range(len(rdr)):
+    line = rdr.iloc[i,:]
+    print(line)
+    if line[0] == 'title' or float(line[1].split(' ')[0]) > 2:
+        print(line[0])
     else:
+        print('123123123')
         print(str(line))
-        temp = pd.DataFrame({'memo':str(line[0])},index=[0])
+        temp = pd.DataFrame({'memo':str(line[2])},index=[0])
+        print('123123123')
         print(temp)
         temp['memo'] = temp['memo'].str.replace(pat=r'[^A-Za-z0-9]', repl=r' ', regex=True)
         temp['memo'] = temp['memo'].str.replace(pat=r'[\s\s+]', repl=r' ', regex=True)
         temp = temp['memo'][0].lower().split()
+
+        print('1231231234')
         print(temp)
+
         for i in temp:
             if i in word:
-                word[i] = word[i] + int(line[2].replace(',',''))
+                word[i] = word[i] + 1
             else:
                 word[i] = 1
 
+
+print(word)
+word = sorted(word.items(),reverse=True,key=lambda item:item[1])
+with open('./'+keyword+'_1.txt','w',-1,'utf-8',newline='') as f:
+    wt = csv.writer(f)
+    wt.writerows(word)
+"""
 #del word[keyword]
 del word['table']
 del word['living']
@@ -37,7 +52,7 @@ del word['the']
 del word['with']
 del word['and']
 del word['in']
-word = sorted(word.items(),reverse=True,key=lambda item:item[1])
+
 
 n_word = 0
 etc = 0
@@ -70,6 +85,5 @@ plt.axis('equal')
 plt.title('Pie Chart of Result : '+keyword )
 plt.show()
 
-with open('./'+keyword+'_1.txt','w',-1,'utf-8',newline='') as f:
-    wt = csv.writer(f)
-    wt.writerows(word)
+
+"""
